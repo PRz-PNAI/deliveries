0774aa48c3651269e04d0777f8b0bb0b98b3b48d
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-27 11:54:28 +0200

    ClientDetailsController implemented

--- app/controllers/client_details_controller.rb
+++ app/controllers/client_details_controller.rb
@@ -1,2 +1,22 @@
 class ClientDetailsController < ApplicationController
+  def edit
+    @client_details = ClientDetailForm.new
+    @client_details.client = Client.find(params[:id])
+  end
+
+  def update
+    @client_details = ClientDetailForm.new(client_params)
+    @client_details.id = params[:id]
+    if @client_details.valid?
+      logger.warn "TODO: #{@client_details}"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def client_params
+    params.require(:client_detail).permit(:name, :nip, :city, :details)
+  end
 end
