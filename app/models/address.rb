bfb2bbb6eeac7f7ed88aedee7caf308aff134230
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-27 09:56:58 +0200

    Added references between client and address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,7 @@
 class Address < ApplicationRecord
+  belongs_to :client
+
+  def to_s
+    "#{city}, #{details}"
+  end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,8 @@
 class Client < ApplicationRecord
+  has_many :addresses
+
+  def address
+    return "" if addresses.empty?
+    addresses.where(valid_to: nil).first
+  end
 end
